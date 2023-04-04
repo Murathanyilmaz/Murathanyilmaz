338d633b00576dbbfc7109a4490cb661dfbde81c
Murathan <97229162+Murathanyilmaz@users.noreply.github.com>
2023-04-04 15:29:41 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,48 +3,32 @@ const jumbleButton = document.getElementById("jumbleButton");
 const diceButton = document.getElementById("diceButton");
 const guessButton = document.getElementById("guessButton");
 const portfolioButton = document.getElementById("portfolioButton");
-const contactButton = document.getElementById("contactButton");
-const cvButton = document.getElementById("cvButton");
 
 jumbleButton.addEventListener("click", () => {
     document.querySelector(".portfolio").classList.add("hidden");
     document.querySelector(".pig-game").classList.add("hidden");
+    document.querySelector(".pig-game-desc").classList.add("hidden");
     document.querySelector(".jumble-game").classList.remove("hidden");
     document.querySelector(".guess-game").classList.add("hidden");
-    document.querySelector(".cv").classList.add("hidden"); document.querySelector(".contact").classList.add("hidden");
 })
 diceButton.addEventListener("click", () => {
     document.querySelector(".portfolio").classList.add("hidden");
     document.querySelector(".pig-game").classList.remove("hidden");
+    document.querySelector(".pig-game-desc").classList.remove("hidden");
     document.querySelector(".jumble-game").classList.add("hidden");
     document.querySelector(".guess-game").classList.add("hidden");
-    document.querySelector(".cv").classList.add("hidden"); document.querySelector(".contact").classList.add("hidden");
 })
 guessButton.addEventListener("click", () => {
     document.querySelector(".portfolio").classList.add("hidden");
     document.querySelector(".pig-game").classList.add("hidden");
+    document.querySelector(".pig-game-desc").classList.add("hidden");
     document.querySelector(".jumble-game").classList.add("hidden");
     document.querySelector(".guess-game").classList.remove("hidden");
-    document.querySelector(".cv").classList.add("hidden"); document.querySelector(".contact").classList.add("hidden");
 })
 portfolioButton.addEventListener("click", () => {
     document.querySelector(".portfolio").classList.remove("hidden");
     document.querySelector(".pig-game").classList.add("hidden");
+    document.querySelector(".pig-game-desc").classList.add("hidden");
     document.querySelector(".jumble-game").classList.add("hidden");
     document.querySelector(".guess-game").classList.add("hidden");
-    document.querySelector(".cv").classList.add("hidden"); document.querySelector(".contact").classList.add("hidden");
-})
-cvButton.addEventListener("click", () => {
-    document.querySelector(".portfolio").classList.add("hidden");
-    document.querySelector(".pig-game").classList.add("hidden");
-    document.querySelector(".jumble-game").classList.add("hidden");
-    document.querySelector(".guess-game").classList.add("hidden");
-    document.querySelector(".cv").classList.remove("hidden"); document.querySelector(".contact").classList.add("hidden");
-})
-contactButton.addEventListener("click", () => {
-    document.querySelector(".portfolio").classList.add("hidden");
-    document.querySelector(".pig-game").classList.add("hidden");
-    document.querySelector(".jumble-game").classList.add("hidden");
-    document.querySelector(".guess-game").classList.add("hidden");
-    document.querySelector(".cv").classList.add("hidden"); document.querySelector(".contact").classList.remove("hidden");
 })
