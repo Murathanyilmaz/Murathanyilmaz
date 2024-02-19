a2c8b03fcd50115b2604e88df67af287609890fd
Murathan <97229162+Murathanyilmaz@users.noreply.github.com>
2024-02-19 10:20:02 +0000

    Updated

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 "use strict"
 const portfolioButton = document.getElementById("portfolioButton");
+const unitybutton = document.getElementById("unitybutton");
 const jumbleButton = document.getElementById("jumbleButton");
 const diceButton = document.getElementById("diceButton");
 const guessButton = document.getElementById("guessButton");
 const memoryGameButton = document.getElementById("memoryGameButton");
 
 portfolioButton.addEventListener("click", () => {
+    document.querySelector(".portfolio").classList.remove("hidden");
+    document.querySelector(".jumble-game").classList.add("hidden");
+    document.querySelector(".pig-game").classList.add("hidden");
+    document.querySelector(".pig-game-desc").classList.add("hidden");
+    document.querySelector(".guess-game").classList.add("hidden");
+    document.querySelector(".memorygame").classList.add("hidden");
+})
+unitybutton.addEventListener("click", () => {
     document.querySelector(".portfolio").classList.remove("hidden");
     document.querySelector(".jumble-game").classList.add("hidden");
     document.querySelector(".pig-game").classList.add("hidden");
